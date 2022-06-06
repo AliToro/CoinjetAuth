35ea5807185e79087eb794ae390ce38575462864
Alireza Haghighat <alireza.haghighat@zalando.de>
2022-06-06 20:55:31 +0200

    Update user model in DB

--- app/db_models.py
+++ app/db_models.py
@@ -7,6 +7,14 @@ class User(Base):
     __tablename__ = "user"
 
     id = Column(Integer, primary_key=True, index=True)
+    phone = Column(String, unique=True, index=True)
+    role = Column(Integer)
     email = Column(String, unique=True, index=True)
+    firstname = Column(String)
+    lastname = Column(String)
+    avatar = Column(String)
+    telegram_id = Column(String)
+    telegram_username = Column(String)
+    telegram_chat_id = Column(String)
+    username = Column(String)
     password = Column(String)
-    is_admin = Column(Boolean, default=False)
