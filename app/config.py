87aa6acfa414c218a107d222bebae2d0a1978afb
Alireza Haghighat <alireza.haghighat@zalando.de>
2022-06-12 19:36:32 +0200

    Change database to Postgresql

--- app/config.py
+++ app/config.py
@@ -1,9 +1,17 @@
+import os
+
 from pydantic import BaseSettings
+
+user = os.environ['coinjet_db_user']
+password = os.environ['coinjet_db_password']
+host = os.environ['coinjet_db_host']
+db_name = os.environ['coinjet_db_dbname']
 
 
 class Settings(BaseSettings):
     secret: str  # autmatically taken from environement variable
-    database_uri: str = "sqlite:///../files/app.db"
+    # database_uri: str = "sqlite:///../files/app.db"
+    database_uri: str = "postgresql://{}:{}@{}/{}".format(user, password, host, db_name)
     token_url: str = "/auth/token"
 
 
--- app/db.py
+++ app/db.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 from config import DEFAULT_SETTINGS
 
-engine = create_engine(DEFAULT_SETTINGS.database_uri, connect_args={"check_same_thread": False})
+engine = create_engine(DEFAULT_SETTINGS.database_uri)
 Base = declarative_base(bind=engine)
 db_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
